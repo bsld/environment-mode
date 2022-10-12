f1d7ec7ffa402e2175f2e7e14d76ccae0d3f3688
bsld <miromax911@inbox.ru>
2022-10-12 21:31:11 +0300

    Remove redundant interface implementation

--- src/main/java/com/company/environmentmodel/environment/member/animals/Herbivore.java
+++ src/main/java/com/company/environmentmodel/environment/member/animals/Herbivore.java
@@ -1,7 +1,5 @@
 package com.company.environmentmodel.environment.member.animals;
 
-import com.company.environmentmodel.environment.member.Eatable;
-
-public abstract class Herbivore extends Animal implements Eatable {
+public abstract class Herbivore extends Animal {
 
 }
