4049c450a9dd014c738789385cb8d253262c07dd
bsld <miromax911@inbox.ru>
2022-10-23 18:56:28 +0300

    Improve cell placement

--- src/main/java/com/company/environmentmodel/environment/Environment.java
+++ src/main/java/com/company/environmentmodel/environment/Environment.java
@@ -1,5 +1,6 @@
 package com.company.environmentmodel.environment;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.company.environmentmodel.environment.member.EnvironmentMember;
@@ -11,8 +12,9 @@ import com.company.environmentmodel.environment.member.plants.Carrot;
 
 public class Environment {
     private int width, height;
-    private int occupiedPoints;
-    private int totalPoints;
+    // private int occupiedCells;
+    private int totalCells;
+    private ArrayList<Tuple> unoccupiedCells;
     // private ArrayList<EnvironmentMember> members;
 
     private Cell[][] cells;
@@ -25,9 +27,15 @@ public class Environment {
     public void setSize(int width, int height) {
         this.width = width;
         this.height = height;
-        this.occupiedPoints = 0;
-        this.totalPoints = width * height;
+        this.totalCells = width * height;
         this.cells = new Cell[width][height];
+
+        this.unoccupiedCells = new ArrayList<>(this.totalCells);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.unoccupiedCells.add(new Tuple(i, j));
+            }
+        }
     }
 
     public int getWidth() {
@@ -38,12 +46,12 @@ public class Environment {
         return height;
     }
 
-    public int getOccupiedPoints() {
-        return occupiedPoints;
+    public int getOccupiedCells() {
+        return totalCells - unoccupiedCells.size();
     }
 
-    public int getTotalPoints() {
-        return totalPoints;
+    public int getTotalCells() {
+        return totalCells;
     }
 
     public Cell getCell(int i, int j) {
@@ -51,27 +59,28 @@ public class Environment {
     }
 
     public void reset() {
-        this.occupiedPoints = 0;
+        this.unoccupiedCells = new ArrayList<>(this.totalCells);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.unoccupiedCells.add(new Tuple(i, j));
+            }
+        }
+        
         this.cells = new Cell[width][height];
     }
 
     public boolean addMember(EnvironmentMember member) {
-        if (this.occupiedPoints == this.totalPoints) {
+        if (this.getOccupiedCells() == this.totalCells) {
             return false;
         }
 
-        boolean placed = false;
-
-        int randomX, randomY;
         while (true) {
-            randomX = ThreadLocalRandom.current().nextInt(0, width);
-            randomY = ThreadLocalRandom.current().nextInt(0, height);
+            int n = ThreadLocalRandom.current().nextInt(0, unoccupiedCells.size());
+            Tuple t = unoccupiedCells.get(n);
 
-            if (this.cells[randomX][randomY] == null) {
-                this.cells[randomX][randomY] = new Cell(randomX, randomY, member);
-                placed = true;
-                this.occupiedPoints++;
-
+            if (this.cells[t.x][t.y] == null) {
+                this.cells[t.x][t.y] = new Cell(t.x, t.y, member);
+                unoccupiedCells.remove(n);
                 break;
             }
         }
@@ -126,5 +135,14 @@ public class Environment {
             }
             System.out.println();
         }
+    }
+}
+
+class Tuple {
+    public int x, y;
+
+    public Tuple(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
